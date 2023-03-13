4aae5b802a6ccd783fe55e7b9db91125145b24cb
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-03-13 10:12:09 +0500

    Added all the fonts and some ...

--- lib/screens/welcome/view.dart
+++ lib/screens/welcome/view.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class WelcomeScreen extends GetView {
@@ -8,10 +9,40 @@ class WelcomeScreen extends GetView {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Welcome Screen"),
-      ),
+    return  Scaffold(
+      body:SizedBox(
+        height: 780.h,
+        width: 360.w,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView(
+              scrollDirection: Axis.horizontal,
+              reverse: false,
+              onPageChanged: (value) {
+                // 
+              },
+              controller: PageController(
+                initialPage: 0,
+                keepPage: false,
+                viewportFraction: 1,
+
+              ),
+              pageSnapping: false,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage('assets/images/'))
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 } 
